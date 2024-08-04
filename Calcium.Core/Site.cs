574a2c55af538d81cdb9141b12d17daeb47603c6
Vidush H. Namah <vidush.namah@orion-next.com>
2024-08-04 15:29:13 +0400

    Use optimized emojis

--- Calcium.Core/Site.cs
+++ Calcium.Core/Site.cs
@@ -5,7 +5,7 @@ namespace Calcium.Core;
 public static class Site
 {
     public readonly static string Title = "Calcium Site";
-    public readonly static string ResourcesBaseURL = "https://raw.githubusercontent.com/vnsnippets/developers-conference/release/resources/.resources";
+    public readonly static string ResourcesBaseURL = "https://raw.githubusercontent.com/vnsnippets/developers-conference/release/resources/.optimized";
     public static class Brand
     {
         public readonly static string Label = "Developers Conference";

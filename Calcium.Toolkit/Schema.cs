ff52d9ca931415ed2a3beb98f9d4813a652e0373
Vidush H. Namah <vidush.namah@orion-next.com>
2024-08-29 22:30:56 +0400

    Adapt code base to new Calcium Oxide component parameters

--- Calcium.Toolkit/Schema.cs
+++ Calcium.Toolkit/Schema.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using Microsoft.AspNetCore.Components;
+using System.Xml.Linq;
 
 namespace Calcium.Toolkit.Schema
 {
@@ -11,6 +12,12 @@ namespace Calcium.Toolkit.Schema
         LinkedIn,
         X
     }
+    public struct Brand
+    {
+        public string BaseURL { get; set; }
+        public string Label { get; set; }
+        public string ImagePath { get; set; }
+    };
 
     public class BaseLink(string name, string url, string? type = null)
     {
@@ -19,11 +26,17 @@ namespace Calcium.Toolkit.Schema
         public string Name { get; set; } = name;
         public string URL { get; set; } = url ?? string.Empty;
         public string? Type { get; set; } = type;
+        public RenderFragment? IconFragment { get; set; }
     }
 
     public class SocialLink(string name, string url, SocialMediaTypes icon) : BaseLink(name, url)
     {
         public SocialMediaTypes Icon { get; set; } = icon;
+    }
+
+    public class Footer
+    {
+        public string? Message { get; set; }
     }
 
     namespace Layouts
@@ -41,23 +54,24 @@ namespace Calcium.Toolkit.Schema
     {
         public class Hero
         {
-            public string[] Title { get; set; } = [];
+            public string? Title { get; set; }
             public string[] Captions { get; set; } = [];
             public BaseLink[] CTA { get; set; } = [];
         }
 
-        public class BaseContent
+        public class BaseContent(string title, string caption)
         {
             public int Index { get; set; }
-            public string Title { get; set; } = string.Empty;
-            public string Caption { get; set; } = string.Empty;
+            public string Title { get; set; } = title;
+            public string Caption { get; set; } = caption;
             public string? Link { get; set; }
             public bool Highlight { get; set; } = false;
         }
 
-        public class ContentWithIcon : BaseContent
+        public class ContentWithIcon(string title, string caption) : BaseContent(title, caption)
         {
             public string? Icon { get; set; }
+            public RenderFragment? IconFragment { get; set; }
         }
     }
 }
--- Calcium.Web/Site.cs
+++ Calcium.Web/Site.cs
@@ -1,13 +1,19 @@
 ﻿using Calcium.Toolkit.Oxide.Widgets.Schema;
+using Calcium.Toolkit.Schema;
 using Calcium.Toolkit.Schema.Layouts;
 using Calcium.Toolkit.Schema.Sections;
+using Calcium.Web.Assets;
+using System.Threading.Channels;
 
 namespace Calcium.Web
 {
     public class Site
     {
+
         public readonly static string Title = "Developers Conference 2025";
-        public readonly static string ResourcesBaseURL = "https://raw.githubusercontent.com/vnsnippets/developers-conference/release/resources/.resources";
+        //public readonly static string ResourcesBaseURL = "https://raw.githubusercontent.com/vnsnippets/developers-conference/release/resources/.resources";
+        public readonly static string ResourcesBaseURL = "https://raw.githubusercontent.com/vnsnippets/developers-conference/release/resources/.optimized";
+        public static string EmojiPath(string path) => $"{ResourcesBaseURL}/{path}";
 
         public readonly struct Brand
         {
@@ -21,15 +27,27 @@ namespace Calcium.Web
             public readonly static string Message = "MSCC DEVELOPERS CONFERENCE 2025";
         }
 
+        public readonly struct Menus
+        {
+
+            public readonly static List<BaseLink> Socials = [
+                new("facebook", "https://facebook.com") {
+                    IconFragment = Social.Facebook
+                },
+                new("instagram", "https://instagram.com") {
+                    IconFragment = Social.Instagram
+                },
+                new("linkedin", "https://linkedin.com") {
+                    IconFragment = Social.LinkedIn
+                }
+            ];
+        }
+
         public readonly struct Sections
         {
             public readonly static Hero Hero = new()
             {
-                Title = [
-                    "Devel#{Smilies/Face with Open Mouth | O}#pers",
-                    "C#{Smilies/Nerd Face | O}#nference",
-                    "2025"
-                ],
+                Title = "Devel#{Smilies/Face with Open Mouth | O}#pers C#{Smilies/Nerd Face | O}#nference 2025",
                 Captions = [
                     "24 - 26 July 2025",
                     "Caudan Art Centre"
@@ -46,21 +64,17 @@ namespace Calcium.Web
                 Title = "Want to be a part of it?",
                 Caption = "Our conference is a community event, built and organised by the community for the community. You can help make it happen as a sponsor or a speaker.",
                 Content = [
-                    new() {
-                    Icon = "Smilies/Star-Struck.png",
-                    Highlight = true,
-                    Title = "Sponsor the event!",
-                    Caption = "Our conference has always been free of charge - and that's only possible with your support. Become a sponsor and grab your front-row seat as you help us organize the next one.",
-                    Link = "mailto:vidush.namah@orion-next.com",
-                    Index = 0
-                },
-                new() {
-                    Icon = "Smilies/Grinning Face with Big Eyes.png",
-                    Title = "Submit a session",
-                    Caption = "We are excited to see what kind of talks will come up this year! If you are interested in joining the conference as a speaker, submit your session proposal through our call for proposals.",
-                    Link = "#",
-                    Index = 1
-                }
+                    new("Sponsor the event!", "Our conference has always been free of charge - and that's only possible with your support. Become a sponsor and grab your front-row seat as you help us organize the next one.") {
+                        Icon = EmojiPath("Smilies/Star-Struck.png"),
+                        Highlight = true,
+                        Link = "mailto:vidush.namah@orion-next.com",
+                        Index = 0
+                    },
+                    new("Submit a session", "We are excited to see what kind of talks will come up this year! If you are interested in joining the conference as a speaker, submit your session proposal through our call for proposals.") {
+                        Icon = EmojiPath("Smilies/Grinning Face with Big Eyes.png"),
+                        Link = "#",
+                        Index = 1
+                    }
                 ]
             };
 
@@ -69,31 +83,23 @@ namespace Calcium.Web
                 Title = "Let's have a blast!",
                 Caption = "Our community and team absolutely loved the energy in the last conference. We are looking forward to your participation in the next one!",
                 Content = [
-                    new() {
-                    Index = 0,
-                    Highlight = true,
-                    Title = "1600+",
-                    Caption = "Attendees",
-                    Icon = "Smilies/Partying Face.png"
-                },
-                new() {
-                    Index = 1,
-                    Title = "80+",
-                    Caption = "Speakers",
-                    Icon = "People with professions/Technologist Medium Skin Tone.png"
-                },
-                new() {
-                    Index = 2,
-                    Title = "70+",
-                    Caption = "Sessions",
-                    Icon = "Travel and places/Rocket.png"
-                },
-                new() {
-                    Index = 3,
-                    Title = "20+",
-                    Caption = "Sponsors",
-                    Icon = "Smilies/Smiling Face with Hearts.png"
-                }
+                    new("1600+", "Attendees") {
+                        Index = 0,
+                        Highlight = true,
+                        Icon = EmojiPath("Smilies/Partying Face.png")
+                    },
+                    new("80+", "Speakers") {
+                        Index = 1,
+                        Icon = EmojiPath("People with professions/Technologist Medium Skin Tone.png")
+                    },
+                    new("70+", "Sessions") {
+                        Index = 2,
+                        Icon = EmojiPath("Travel and places/Rocket.png")
+                    },
+                    new("20+", "Sponsors") {
+                        Index = 3,
+                        Icon = EmojiPath("Smilies/Smiling Face with Hearts.png")
+                    }
                 ]
             };
 
@@ -102,49 +108,37 @@ namespace Calcium.Web
                 Title = "What to expect?",
                 Caption = "Learn about industry trends, best practices and new innovations through sessions and workshops when you are not having fun with all the entertainment and activities.",
                 Content = [
-                    new() {
-                    Title = "Workshops",
-                    Caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    Icon = "Activities/Artist Palette.png"
-                },
-                new() {
-                    Title = "Panel Discussions",
-                    Caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    Icon = "Activities/Bullseye.png"
-                },
-                new() {
-                    Title = "Speaker Sessions",
-                    Caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    Icon = "Activities/Performing Arts.png"
-                },
-                new() {
-                    Title = "Friendly Atmosphere",
-                    Caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    Icon = "Activities/Bowling.png"
-                },
-                new() {
-                    Title = "Activities",
-                    Caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    Icon = "Activities/Sports Medal.png"
-                },
-                new() {
-                    Title = "Networking",
-                    Caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    Icon = "Objects/Necktie.png"
-                }
+                    new("Workshops", "Led by industry experts, these workshops offer a unique opportunity to gain practical skills and insights, making them a valuable experience for both beginners and seasoned professionals.") {
+                        Icon = EmojiPath("Activities/Artist Palette.png")
+                    },
+                    new("Panel Discussions", "The panel discussions bring together thought leaders and innovators to explore current trends, challenges, and the future of technology. These engaging sessions provide a platform for diverse perspectives and lively debates, sparking new ideas and inspiring our community.") {
+                        Icon = EmojiPath("Activities/Bullseye.png")
+                    },
+                    new("Speaker Sessions", "DevCon sessions are packed with knowledge-sharing and inspiration, featuring local and international speakers. From cutting-edge innovations to practical applications, these talks cover a wide range of topics, ensuring there's something for everyone, whether you're a developer, designer, or entrepreneur.") {
+                        Icon = EmojiPath("Activities/Performing Arts.png")
+                    },
+                    new("Friendly Atmosphere", "DevCon is known for its warm and inclusive atmosphere. Whether you are a first-time attendee or a conference veteran, you'll feel right at home. Our community is supportive and eager to connect, making DevCon a place where lasting professional relationships are formed.") {
+                        Icon = EmojiPath("Activities/Bowling.png")
+                    },
+                    new("Activities", "Beyond the sessions and discussions, DevCon offers a range of cool activities that add an extra layer of fun to the conference. From interactive games to creative challenges, these activities are designed to engage and entertain, making DevCon a memorable experience for all attendees.") {
+                        Icon = EmojiPath("Activities/Sports Medal.png")
+                    },
+                    new("Networking", "The networking hour is an 'invitation-only' event, offering a relaxed environment to connect with peers, mentors, and industry leaders. It's the perfect opportunity to exchange ideas, collaborate on projects, and expand your professional network in a casual, friendly setting.") {
+                        Icon = EmojiPath("Objects/Necktie.png")
+                    }
                 ]
             };
 
             public readonly static string[] Gallery =
             [
                 "images/Gallery-1.webp",
-            "images/Gallery-2.webp",
-            "images/Gallery-3.webp",
-            "images/Gallery-4.webp",
-            "images/Gallery-5.webp",
-            "images/Gallery-6.webp",
-            "images/Gallery-7.webp",
-            "images/Gallery-8.webp"
+                "images/Gallery-2.webp",
+                "images/Gallery-3.webp",
+                "images/Gallery-4.webp",
+                "images/Gallery-5.webp",
+                "images/Gallery-6.webp",
+                "images/Gallery-7.webp",
+                "images/Gallery-8.webp"
             ];
         }
 
@@ -175,7 +169,7 @@ namespace Calcium.Web
                     "Be the first to know when registration for the conference opens next year.",
                 "An invitation will be delivered straight to your inbox 😉"
                 ],
-                Icon = "Smilies/Smiling Face with Heart-Eyes.png",
+                Icon = EmojiPath("Smilies/Smiling Face with Heart-Eyes.png"),
                 Placeholder = "Enter your email address",
                 CTA = "Join Waitlist"
             };
